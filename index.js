38ef153be076c99c69c9a1e3ffa9c56c5a1be1bd
AYUSHI GUPTA <89902111+ag112002@users.noreply.github.com>
2021-09-09 19:24:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+        document.write("HELLO AYUSHI");
+        //console.API
+        console.log("Hi")
+        console.warn("danger");
+        alert("me");
+        console.error("error");
+        console.assert(4==6);
+       // variables in JS-Containers to store data values
+       //Data types are of 2 types
+       //primitive:und,boolean,string,number,null,symbol
+       //   reference  :arrays and objects
+       //1. Numbers
+       var num1=2;
+       var num2=1;
+       console.log(num1,num2);
+       //2. Strings
+       var string1="hi";
+       var string2='hi';
+       console.log(string1,string2);
+       //3. objects
+       var marks= {
+           ravi:43,
+           ashi:98,
+       }
+       console.log(marks);
+       //4.boolean
+       var a=true;
+       var b=false;
+       console.log(a,b);
+       //undefined
+       var und;
