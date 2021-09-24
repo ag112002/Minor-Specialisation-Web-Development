9247f1aabdbe50b6305126331d02270f19f767a9
AYUSHI GUPTA <89902111+ag112002@users.noreply.github.com>
2021-09-24 23:47:22 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,sm[][],c=0;
+    int a[4][5]={
+                 {0,0,1,0,0},
+                 {0,10,6,0,0},
+                 {0,0,3,0,0},
+                 {0,0,5,0,0}
+        
+                };
+    for(i=0 ; i<4 ; i++)
+    {
+        for(j=0; j<5 ; j++)
+        {
+            if(a[i][j]!=0)
+            c++;
+        }
+    }
+    s[0][0]=4;
+    s[0][1]=5;
+    s[0][2]=c;
+    for(i=0 ; i<)
+    {
+        for(j=0 ; j<3 ; j++)
+        {
+            
+        }
+    }
+
+    return 0;
+}
