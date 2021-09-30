9f7f3f9686c295df347e2ed5825061c0e2e68cd7
AYUSHI GUPTA <89902111+ag112002@users.noreply.github.com>
2021-09-30 23:30:08 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int fact,x,sum=0,a,n;
+    cin<<x<<n;
+    
+    while(n);
+{
+    cin<<a;
+    fact=1;
+    for(i=a ; i>=1 ; i--)
+    
+   { fact=fact*a;
+   }
+
+   for(i=0 ; i<=n ; i++)
+   {
+       sum+=fact(a)/x^a;
+   }
+   
+}
+cout>>sum;
+}
+
